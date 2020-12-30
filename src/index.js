e7a56727d069a1ff1c3b555d310e5a5b1d063371
Jason L Perry <jason@suncoast.io>
2020-12-30 04:34:37 -0500

    Hello, Electron

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { app, Menu, Tray, nativeImage } = require('electron')
+const path = require('path')
+
+app.dock.hide()
+
+let tray
+
+
+app.whenReady().then(() => {
+  const image = nativeImage.createFromPath(path.resolve(__dirname, '../res/trayTemplate.png'))
+  tray = new Tray(image)
+  
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: 'Quit', click: () => {
+        app.isQuiting = true
+        app.quit()
+      }
+    }
+  ])
+  tray.setToolTip('AttaQR')
+  tray.setContextMenu(contextMenu)
+})
+
+// Handle creating/removing shortcuts on Windows when installing/uninstalling.
+if (require('electron-squirrel-startup')) {
+  app.quit()
+}
