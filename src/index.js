055abbd5e5786cfa41bfcc3e3c00b5a684a571e6
Jason L Perry <jason@suncoast.io>
2021-01-02 03:39:11 -0500

    Use different icon on windows for tray

--- src/index.js
+++ src/index.js
@@ -195,7 +195,7 @@ function checkForScreenAccess() {
 
 app.whenReady().then(() => {
   const image = nativeImage.createFromPath(
-    path.resolve(__dirname, '../res/trayTemplate.png')
+    path.resolve(__dirname, isMacOS ? '../res/trayTemplate.png' : '../res/icons.icon.ico')
   )
   tray = new Tray(image)
 
